85740ee750f9a9e3b0715b7f500bb37c65034cae
amitkhiani <khiani.amit@gmail.com>
2019-04-30 12:56:19 -0400

    Added HelloController.java

---
+++ src/main/java/com/example/demoapp/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demoapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/")
+    public String helloWorld() {
+        return "Hello from Spring!";
+    }
+}
