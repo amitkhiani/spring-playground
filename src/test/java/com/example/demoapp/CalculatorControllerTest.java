763c9167bff2e25a9a995c756a6809d0193a430b
amitkhiani <khiani.amit@gmail.com>
2019-05-03 17:09:21 -0400

    Refactoring the code into MathService

--- src/test/java/com/example/demoapp/CalculatorControllerTest.java
+++ src/test/java/com/example/demoapp/CalculatorControllerTest.java
@@ -1,12 +1,16 @@
 package com.example.demoapp;
 
+import com.example.demoapp.service.MathService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -18,8 +22,13 @@ public class CalculatorControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private MathService mathService;
+
     @Test
     public void testForAddOperationForCalculator() throws Exception {
+        when(mathService.getResultForOperation(any())).thenReturn("4 + 6 = 10");
+
         this.mvc.perform(get("/math/calculate?operation=add&x=4&y=6"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("4 + 6 = 10"));
@@ -27,6 +36,8 @@ public class CalculatorControllerTest {
 
     @Test
     public void testForAddOperationForCalculatorWhenOperationQueryStringIsAbsent() throws Exception {
+        when(mathService.getResultForOperation(any())).thenReturn("4 + 6 = 10");
+
         this.mvc.perform(get("/math/calculate?x=4&y=6"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("4 + 6 = 10"));
@@ -34,6 +45,8 @@ public class CalculatorControllerTest {
 
     @Test
     public void testForMultiplyOperationForCalculator() throws Exception {
+        when(mathService.getResultForOperation(any())).thenReturn("4 * 6 = 24");
+
         this.mvc.perform(get("/math/calculate?operation=multiply&x=4&y=6"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("4 * 6 = 24"));
@@ -41,6 +54,8 @@ public class CalculatorControllerTest {
 
     @Test
     public void testForSubtractOperationForCalculator() throws Exception {
+        when(mathService.getResultForOperation(any())).thenReturn("4 - 6 = -2");
+
         this.mvc.perform(get("/math/calculate?operation=subtract&x=4&y=6"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("4 - 6 = -2"));
@@ -48,6 +63,8 @@ public class CalculatorControllerTest {
 
     @Test
     public void testForDivisionOperationForCalculator() throws Exception {
+        when(mathService.getResultForOperation(any())).thenReturn("30 / 5 = 6");
+
         this.mvc.perform(get("/math/calculate?operation=divide&x=30&y=5"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("30 / 5 = 6"));
@@ -55,6 +72,8 @@ public class CalculatorControllerTest {
 
     @Test
     public void testForSumOperationForCalculatorWithTwoValues() throws Exception {
+        when(mathService.getSumForOperation(any())).thenReturn("4 + 5 = 9");
+
         this.mvc.perform(post("/math/sum?n=4&n=5"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("4 + 5 = 9"));
@@ -62,6 +81,8 @@ public class CalculatorControllerTest {
 
     @Test
     public void testForSumOperationForCalculatorWithThreeValues() throws Exception {
+        when(mathService.getSumForOperation(any())).thenReturn("4 + 5 + 6 = 15");
+
         this.mvc.perform(post("/math/sum?n=4&n=5&n=6"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("4 + 5 + 6 = 15"));
@@ -69,6 +90,8 @@ public class CalculatorControllerTest {
 
     @Test
     public void testForSumOperationForCalculatorWithFourValues() throws Exception {
+        when(mathService.getSumForOperation(any())).thenReturn("4 + 5 + 6 + 7 = 22");
+
         this.mvc.perform(post("/math/sum?n=4&n=5&n=6&n=7"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("4 + 5 + 6 + 7 = 22"));
